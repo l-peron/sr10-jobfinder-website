a7b28063d740387facf639248d95c1318bcc1b35
Léo Peron <leoperon@peronleo01-hz.etu.utc>
2023-06-05 17:20:11 +0200

    index

--- TD3_NODE/workys/routes/index.js
+++ TD3_NODE/workys/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var fichePosteModel = require('../models/fiche_postes.js');
+var offresEmploiModel = require('../models/offre_emplois.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,8 +11,28 @@ router.get('/', function(req, res, next) {
 
   const query_string = `%${req.query.q || ''}%`
   
-  fichePosteModel.readall(function(result) {
-    res.render('index', { user: req.session.user, annonces: result });
+  offresEmploiModel.readAllWithExtendedInfos(function(result) {
+
+    const annonces = result.filter(a => { return new Date() < new Date(a.valid_date) && a.status == 'published'; }).map(a => {
+      return {
+        id: a.id,
+        description: a.description,
+        title: a.title,
+        poste_status: a.poste_status,
+        type: a.type,
+        address: a.poste_description,
+        responsable: a.responsable,
+        max_salary: a.max_salary,
+        min_salary: a.min_salary,
+        avg_salary: a.average_salary,
+        hours: a.hours,
+        day_off: a.day_off,
+        org_name: a.name
+      }
+    });
+
+    console.log(annonces);
+    res.render('index', { user: req.session.user, annonces });
   });
 });
 
